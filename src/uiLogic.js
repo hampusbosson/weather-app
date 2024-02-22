61cc0b35de3fed2a78ec9e87203006371154d6aa
Hampus Bosson <hampus.bosson@outlook.com>
2024-02-22 21:54:17 +0100

    Refactor module into a factory function

--- src/uiLogic.js
+++ src/uiLogic.js
@@ -2,7 +2,8 @@ const UI = (() => {
   const displayLogic = {
     locationText: document.querySelector(".location"),
     tempText: document.querySelector(".temp"),
-    weatherText: document.querySelector(".weather"),
+    weatherText: document.querySelector(".weather-text"),
+    conditionIcon: document.getElementById('condition-image'),
     feelslikeText: document.getElementById("feels-like"),
     precipText: document.getElementById("precip"),
     visibilityText: document.getElementById("visibility"),
@@ -17,12 +18,18 @@ const UI = (() => {
     displayLogic.precipText.textContent = "";
     displayLogic.visibilityText.textContent = "";
     displayLogic.humidityText.textContent = "";
+    displayLogic.conditionIcon.src = ""; 
   };
+
+  const setConditionIcon = function(conditionIcon) {
+    displayLogic.conditionIcon.src = conditionIcon;
+  }
 
   const updateWeatherBox = function (
     location,
     temp,
     condition,
+    conditionIcon,
     feelsLike,
     precip,
     visibility,
@@ -37,6 +44,9 @@ const UI = (() => {
     displayLogic.precipText.textContent = precip;
     displayLogic.visibilityText.textContent = visibility;
     displayLogic.humidityText.textContent = humidity;
+
+    setConditionIcon(conditionIcon);
+
   };
   return { updateWeatherBox };
 })();
