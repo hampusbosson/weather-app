8d1b4a339407dcecdd86e348429f1d95b06492e6
Hampus Bosson <hampus.bosson@outlook.com>
2024-02-24 13:36:29 +0100

    Add eventlistener to the document that loads in default city 'stockholm' when the page first loads

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
-import { loadWeatherData, processWeatherData } from "./apiLogic";
-import { UI } from "./uiLogic";
+import { loadData, loadWeatherData } from "./apiLogic";
 
 const form = document.querySelector(".weatherForm");
 const searchText = document.getElementById("search");
@@ -9,30 +8,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const location = searchText.value;
-  loadWeatherData(location)
-    .then((data) => {
-      if (data) {
-        // Check if data is not undefined due to catch block execution
-        const weatherData = processWeatherData(data);
-        console.log(data);
+  loadData(location);
+});
 
-        UI.updateWeatherBox(
-          weatherData.location,
-          weatherData.temp,
-          weatherData.condition,
-          weatherData.conditionIcon,
-          weatherData.feelsLike,
-          weatherData.precip,
-          weatherData.visibility,
-          weatherData.humidity,
-        );
-
-        UI.updateHourBoxes(weatherData.hourlyData); 
-        UI.updateDailyBoxes(weatherData.dailyData);
-      }
-    })
-    .catch((error) => {
-      // This catch is for handling any errors that might not have been caught by the try/catch inside loadWeatherData
-      console.error("Error processing weather data:", error);
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  loadData('Stockholm');
 });
